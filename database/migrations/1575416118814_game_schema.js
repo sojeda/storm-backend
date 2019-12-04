79f571de41009cd1e35c364994f2498f36bd8be4
Manuel Pascual <manupascual90@gmail.com>
2019-12-03 21:13:54 -0300

    Genera tabla games en DB

--- database/migrations/1575416118814_game_schema.js
+++ database/migrations/1575416118814_game_schema.js
@@ -4,14 +4,21 @@
 const Schema = use('Schema')
 
 class GameSchema extends Schema {
-  up () {
+  up() {
     this.create('games', (table) => {
       table.increments()
+      table.string('title')
+      table.text('description')
+      table.integer('developer_id')
+      table.date('year')
+      table.boolean('apt')
+      table.string('video')
+      table.integer('price')
       table.timestamps()
     })
   }
 
-  down () {
+  down() {
     this.drop('games')
   }
 }
