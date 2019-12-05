dc09446bfb2fbcd5ff34656d00c31dfe505c77c4
Eduardo Perez <edujpp1@gmail.com>
2019-12-05 20:21:22 -0300

    Category endpoint complete

--- app/Controllers/Http/Backoffice/CategoryController.js
+++ app/Controllers/Http/Backoffice/CategoryController.js
@@ -9,7 +9,6 @@ class CategoryController {
 
   async store ({ request, response, auth }) {
     const { name } = request.all()
-
     const category = await Category.create({ name })
 
     response.json({
@@ -23,32 +22,48 @@ class CategoryController {
 
   async show ({ request, response }) {
     const category = await Category.find(request.params.id)
-
     response.json(category)
   }
 
-  update ({ request, response }) {
-    response.json({})
+  async update ({ request, response, params }) {
+    const { id } = params
+    const { name } = request.all()
+    let category = await Category.find(id)
+
+    try {
+      category.merge({ name })
+      await category.save()      
+      response.json({
+        message: 'La categoria se actualizo',
+        category: {
+          id: category.id,
+          name: category.name
+        }
+      })
+    } catch (error) {
+      response.status(500).send(error)
+      console.log(error)
+    }
   }
 
   async destroy ({ request, response, params }) {
-    
     const { id } = params
-    
     let category = await Category.find(id)
 
-    await category.delete()
-
-    category = await Category.find(id)
-
-    if (category==null) {
-      response.json({
-        message: 'Categoria borrada con exito'
+    try {
+      await category.delete()
+      category = await Category.find(id)
+      if (category==null) {
+        response.json({
+          message: 'Categoria borrada con exito'
+        })
+      }
+      response.status(500).json({
+        message: 'No se pudo borrar la categoria'
       })
+    } catch (error) {
+      response.status(500).send(error)
     }
-    response.json({
-      message: 'No se pudo borrar la categoria'
-    })
   }
 }
 
