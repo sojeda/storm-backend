68f421c92c69dc9c981552f277c8a51c55e21537
Alex Robaina <30770802+alexrobaina@users.noreply.github.com>
2019-12-10 20:48:12 -0300

    fix category (#5)

--- app/Controllers/Http/CategoryController.js
+++ app/Controllers/Http/CategoryController.js
@@ -1,11 +1,16 @@
 'use strict'
 
-const Category = use('App/Models/User')
+const Category = use('App/Models/Category')
 
 class CategoryController {
   async index ({ request, response }){
     return await Category.all()
   }
+  
+  async show({ request, response, params }) {
+    return await Category.find(params.id) 
+  }
+
 }
 
 module.exports = CategoryController
