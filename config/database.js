647f8bac44d9bc613e5ef9fb009478a5738ded83
Sergio Ojeda <soj3da@gmail.com>
2019-12-12 20:38:13 -0300

    Fix url

--- config/database.js
+++ config/database.js
@@ -7,6 +7,7 @@ const Env = use('Env')
 const Helpers = use('Helpers')
 
 const Url = require('url-parse')
+const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
 
 module.exports = {
   /*
